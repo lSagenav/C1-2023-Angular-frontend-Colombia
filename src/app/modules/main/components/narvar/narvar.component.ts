475cd9514f90c419517c64369081e6a626656367
lsagenav <jcvanegas@unac.edu.co>
2023-02-23 19:51:20 -0500

    TITULO: decorador @Input DESCRIPCION: se genera y se implemente el decorador @input

--- src/app/modules/main/components/narvar/narvar.component.ts
+++ src/app/modules/main/components/narvar/narvar.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'sofka-narvar',
   templateUrl: './narvar.component.html',
   styleUrls: ['./narvar.component.scss'],
 })
-export class NarvarComponent {}
+export class NarvarComponent {
+  @Input() name: string | undefined;
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() {}
+}
